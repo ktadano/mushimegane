368437fe33362da12fd16021db54c5084556bd6b
ktadano <ktadano@aiming-inc.com>
2017-01-25 16:28:20 +0900

    link to result page

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -1,9 +1,7 @@
 class CompareController < ApplicationController
 
   def create
-    # test 段階なので今はすぐ消しておく
-    # connection.drop_table(table_name)
 
-    redirect_to result
+    redirect_to result_path
   end
 end
