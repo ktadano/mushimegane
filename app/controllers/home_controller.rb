9b6c5ecefebfdd51754a217e0c1dbdccd98af0c7
ktadano <ktadano@aiming-inc.com>
2017-01-25 18:26:25 +0900

    fix home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @file_list = Content.select(:id).pluck(:upload_file_name);
+    @files = Content.all
   end
 
   def create
@@ -13,10 +13,5 @@ class HomeController < ApplicationController
     end
 
    redirect_to root_path
-  end
-
-  def destroy
-    # ファイルとテーブル全て削除する
-    # @connection.drop_table(table_name)
   end
 end
