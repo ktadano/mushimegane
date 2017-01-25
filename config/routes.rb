d65ff817bbfd87833d4e5683c62e87b0292dd952
ktadano <ktadano@aiming-inc.com>
2017-01-25 16:24:57 +0900

    add action home destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   post 'import' => 'home#create'
+  delete 'delete' => 'home#destroy'
   post 'compare' => 'compare#create'
   get 'result' => 'comparison_result#show'
 end
